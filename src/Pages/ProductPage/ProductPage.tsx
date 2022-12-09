cc14f1d00e80f902180aadb2b8b1f49f9bedcfe8
Vladimir <vov4ik.samurai@yandex.ru>
2022-12-09 22:31:54 +0300

    pos abs fs-c for title center

--- src/Pages/ProductPage/ProductPage.tsx
+++ src/Pages/ProductPage/ProductPage.tsx
@@ -1,14 +1,38 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
+import {IDataCatalogs} from "../../Interfaces/IDataCatalogs";
+import {useLocation} from "react-router-dom";
+import {dataCatalogs} from "../../api/Catalogs/catalogs";
 
 const ProductPage: FC = ({}) => {
-    return (
-        <main className="main">
-            <div className="container">
-                <div className="main__content">
+    const [item, setItem] = useState<IDataCatalogs | null>(null);
 
-                </div>
-            </div>
-        </main>
+    const url = useLocation();
+
+
+    useEffect(() => {
+        const urlId = Number(url.pathname.slice(9));
+        console.log(urlId)
+        const current = dataCatalogs[urlId - 1];
+        setItem(current);
+    }, []);
+
+    return (
+        <>
+            {item && (
+                <main className="main">
+                    <div className="container">
+                        <div className="main__content">
+                            <div className="product-page">
+                                <div className="product-page__img">
+                                    <img className="product-page_img" src="" alt=""/>
+                                </div>
+                                .
+                            </div>
+                        </div>
+                    </div>
+                </main>
+            )}
+        </>
     );
 };
 
