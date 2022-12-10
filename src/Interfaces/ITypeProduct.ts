2c19048534f6338193ab24e051ecaf9ec0117d54
Vladimir <vov4ik.samurai@yandex.ru>
2022-12-10 21:09:07 +0300

    crete api type for catalog => optionTypeProduct

---
+++ src/Interfaces/ITypeProduct.ts
@@ -0,0 +1,4 @@
+export interface ITypeProduct {
+    id: string,
+    type: string,
+}
---
+++ src/api/TypeProduct/typeProduct.ts
@@ -0,0 +1,32 @@
+import {ITypeProduct} from "../../Interfaces/ITypeProduct";
+
+export const typeProduct: ITypeProduct[] = [
+    {
+        id: "MIUZ",
+        type: "MIUZ",
+    },
+    {
+        id: "ЭПЛ",
+        type: "ЭПЛ",
+    },
+    {
+        id: "АДАМАС",
+        type: "АДАМАС",
+    },
+    {
+        id: "SOKOLOV",
+        type: "SOKOLOV",
+    },
+    {
+        id: "AVGVST",
+        type: "AVGVST",
+    },
+    {
+        id: "МАРКАЗИТ",
+        type: "МАРКАЗИТ",
+    },
+    {
+        id: "Designer",
+        type: "Designer",
+    },
+]
