b612ce8d4cb1698c7be39e99cb08e9bcaba77321
Vladimir <vov4ik.samurai@yandex.ru>
2022-12-10 21:26:11 +0300

    create search + style

---
+++ src/Components/SearchCatalog/SearchCatalog.tsx
@@ -0,0 +1,24 @@
+import React, {FC} from 'react';
+import "../../Styles/searcgCatalog.scss"
+
+interface Props {
+    search: string;
+    setSearch: React.Dispatch<React.SetStateAction<string>>;
+}
+
+const SearchCatalog: FC<Props> = ({search, setSearch}) => {
+    return (
+        <>
+            <input type="text"
+                   className="search-input"
+                   value={search}
+                   onChange={(e) => {
+                       setSearch(e.target.value)
+                   }}
+                   placeholder="Enter the desired decoration..."
+            />
+        </>
+    );
+};
+
+export default SearchCatalog;
--- src/Pages/CatalogPage/CatalogPage.tsx
+++ src/Pages/CatalogPage/CatalogPage.tsx
@@ -1,15 +1,31 @@
-import React, {FC} from 'react';
+import React, {FC, useEffect, useState} from 'react';
 import {dataCatalogs} from "../../api/Catalogs/catalogs";
 import ProductCardCatalogs from "../../Components/ProductCardCatalogs/ProductCardCatalogs";
 import "../../Styles/catalogPage.scss"
+import SearchCatalog from "../../Components/SearchCatalog/SearchCatalog";
+import {IDataCatalogs} from "../../Interfaces/IDataCatalogs";
 
 const CatalogPage: FC = ({}) => {
+    const [productList, setProductList] = useState<IDataCatalogs[]>([]);
+    const [search, setSearch] = useState<string>('');
+
+    useEffect(() => {
+        const result = dataCatalogs.filter((item) => {
+            return (
+                item.title.toLowerCase().includes(search.toLowerCase())
+            );
+        });
+
+        setProductList(result)
+    }, [search])
+
     return (
         <main className="main">
             <div className="container">
-                <div className="main__content">
+                <div className="main__content_catalog">
+                    <SearchCatalog search={search} setSearch={setSearch}/>
                     <div className="main__content__card">
-                        {dataCatalogs.map((item) => (
+                        {productList.map((item) => (
                             <ProductCardCatalogs item={item} key={item.id}/>
                         ))}
                     </div>
